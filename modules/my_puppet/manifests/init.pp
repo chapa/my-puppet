20a98a7b179c26f3d9300c98d30ff3df714d2ecc
Mickaël Bourgier <mickael.bourgier@gmail.com>
2014-10-31 15:52:24 +0100

    Petite manip pour transformer la remote git du dépôt my-puppet en ssh (en attendant de gérer les dépôts git proprement)

--- modules/my_puppet/manifests/init.pp
+++ modules/my_puppet/manifests/init.pp
@@ -14,5 +14,9 @@ class my_puppet() inherits my_puppet::config {
 		command     => "touch ${install_dir}/.need_to_source_env_sh",
 		refreshonly => true,
 	}
+
+	exec { "cat ${my_puppet::config::install_dir}/.git/config | sed -E 's/url = https?:\\/\\/([a-zA-Z.]+)\\//url = git@\\1:/' > ${my_puppet::config::install_dir}/.git/config":
+		unless => "cat ${my_puppet::config::install_dir}/.git/config | grep 'url = git@'",
+	}
 	
 }
